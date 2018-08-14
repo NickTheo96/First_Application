6d9c7d0b04e0ac23c659bbb9219f3ad3cd04b7ad
NickTheo96 <nicktheodoulou96@gmail.com>
2018-08-14 21:13:20 +0100

    Button Working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
-public class Controller {
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller
+{
+    public Label helloWorld;
+    public void sayHelloWorld(javafx.event.ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
 }
