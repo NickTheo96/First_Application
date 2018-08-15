88027341ed56efbd9c4cbe6c94037b581f7d06ff
NickTheo96 <nicktheodoulou96@gmail.com>
2018-08-15 12:27:04 +0100

    Understood some basics in fxml for the layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application
     {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
